9e5eb802f43f665ecb7271ce24884d6db6edd9e7
newsand <fils.capo@gmail.com>
2019-02-03 16:32:51 -0200

    routs file

--- API/APIRest/app/routes/__init__.py
+++ API/APIRest/app/routes/__init__.py
@@ -1,5 +1,75 @@
 from app import app
+import re
+import pandas as pd
+from app.services import *
 
 @app.route('/')
 def index():
-    return 'RELOU'
+    x = getx()
+    print(x)
+    return 'Hello Cinnecta'
+
+
+@app.route('/vocabulario')
+def vocabulario():
+    return 'RELOUtester'
+
+
+@app.route('/vocabulario2gram')
+def vocabulario2Gram():
+    return 'RELOUtester'
+
+
+@app.route('/wordcount')
+def wordcount():
+    return 'RELOUtester'
+
+@app.route('/wordcount2gram')
+def wordcount2gram():
+    return 'RELOUtester'
+
+
+
+@app.route('/parse')
+def parsse():
+    texto = 'É fácil escrever código. Difícil é escrever código que funcione.'
+    words =split_line(texto)
+    str1 = ''.join(str(e) for e in words)
+    return str1
+
+
+@app.route('/wordCount')
+def wordCount():
+    _text = 'É fácil escrever código. Difícil é escrever código que funcione.'
+    _text= _text.lower()
+    dictio=split_text(_text)
+    d = {'words': [], 'counts': []}
+    for word in dictio:
+        print(word)
+        counts = count_occurrences(word, _text)
+        d['words'].append(word)
+        d['counts'].append(counts)
+    print(d)
+    return 'ok'
+
+def split_line(_text):
+    #split the text
+    words = re.split(' |\.',_text)
+    words = list(filter(None, words))
+    print (words)
+    s = pd.Series(words)
+    print('woot')
+    print(s.value_counts())
+    
+    unique = set(words)
+    print(unique)
+
+    # for each word in the line:
+    # for word in words:
+    #     # print the word
+    #     print(word)
+    return unique
+
+def count_occurrences(word, sentence):
+    return sentence.lower().split().count(word)
+
