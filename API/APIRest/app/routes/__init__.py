78f7a7aa69d6209a45d17a2803b6d479e1a268a0
newsand <fils.capo@gmail.com>
2019-02-03 20:00:26 -0200

    added APIS to 2gram processing

--- API/APIRest/app/routes/__init__.py
+++ API/APIRest/app/routes/__init__.py
@@ -16,6 +16,13 @@ def vocabulario():
     return result
 
 
+@app.route('/vocabulario2gram',methods =['POST'])
+def vocabulario2Gram():
+    wordlist=getherTexts2Gram(request.json)
+    vocabulario=filterUnique(wordlist)
+    result ='\n'.join(vocabulario)
+    return result
+
 
 @app.route('/wordcount',methods =['POST'])
 def wordcount():
@@ -36,6 +43,20 @@ def fullwordcount():
     wordCount=countWords(vocabulario,wordlist)
     result= str(wordCount)
     return result
+
+@app.route('/wordcount2gram',methods =['POST'])
+def wordcount2gram():
+    wordlist=getherTexts2Gram(request.json)
+    vocabulario=filterUnique(wordlist)
+    result=[]
+    for content in request.json:
+        firstSplit=split_text(content['text'])
+        words2Gram=create2Grams(firstSplit)
+        wordCount=countWords(vocabulario,words2Gram)
+        result.append ( content['textname']+ ':'+ wordCount)
+    result= '\n'.join(result)
+    return result
+
 
 def count_occurrences(word, sentence):
     return sentence.lower().split().count(word)
