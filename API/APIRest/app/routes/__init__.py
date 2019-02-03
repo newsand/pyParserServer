7c26176ac1641d9fdd2ec23f4a3a5db1014418ab
newsand <fils.capo@gmail.com>
2019-02-03 19:59:55 -0200

    added APIs to routs

--- API/APIRest/app/routes/__init__.py
+++ API/APIRest/app/routes/__init__.py
@@ -2,73 +2,40 @@ from app import app
 import re
 import pandas as pd
 from app.services import *
+from flask import request
 
 @app.route('/')
 def index():
-    x = getx()
-    print(x)
     return 'Hello Cinnecta'
 
-
-@app.route('/vocabulario')
+@app.route('/vocabulario',methods =['POST'])
 def vocabulario():
-    return 'RELOUtester'
+    wordlist=getherTexts(request.json)
+    vocabulario=filterUnique(wordlist)
+    result ='\n'.join(vocabulario)
+    return result
 
 
-@app.route('/vocabulario2gram')
-def vocabulario2Gram():
-    return 'RELOUtester'
 
-
-@app.route('/wordcount')
+@app.route('/wordcount',methods =['POST'])
 def wordcount():
-    return 'RELOUtester'
+    wordlist=getherTexts(request.json)
+    vocabulario=filterUnique(wordlist)
+    result=[]
+    for content in request.json:
+        words = split_text(content['text'])
+        wordCount=countWords(vocabulario,words)
+        result.append ( content['textname']+ ':'+ wordCount)
+    result= '\n'.join(result)
+    return result
 
-@app.route('/wordcount2gram')
-def wordcount2gram():
-    return 'RELOUtester'
-
-
-
-@app.route('/parse')
-def parsse():
-    texto = 'É fácil escrever código. Difícil é escrever código que funcione.'
-    words =split_line(texto)
-    str1 = ''.join(str(e) for e in words)
-    return str1
-
-
-@app.route('/wordCount')
-def wordCount():
-    _text = 'É fácil escrever código. Difícil é escrever código que funcione.'
-    _text= _text.lower()
-    dictio=split_text(_text)
-    d = {'words': [], 'counts': []}
-    for word in dictio:
-        print(word)
-        counts = count_occurrences(word, _text)
-        d['words'].append(word)
-        d['counts'].append(counts)
-    print(d)
-    return 'ok'
-
-def split_line(_text):
-    #split the text
-    words = re.split(' |\.',_text)
-    words = list(filter(None, words))
-    print (words)
-    s = pd.Series(words)
-    print('woot')
-    print(s.value_counts())
-    
-    unique = set(words)
-    print(unique)
-
-    # for each word in the line:
-    # for word in words:
-    #     # print the word
-    #     print(word)
-    return unique
+@app.route('/fullwordcount',methods =['POST'])
+def fullwordcount():
+    wordlist=getherTexts(request.json)
+    vocabulario=filterUnique(wordlist)
+    wordCount=countWords(vocabulario,wordlist)
+    result= str(wordCount)
+    return result
 
 def count_occurrences(word, sentence):
     return sentence.lower().split().count(word)
